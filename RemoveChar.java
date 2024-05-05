bb84347b9fa9eac92824fe0f1b1f5b1ce3e04658
Subash V <subashvelusamy10@gmail.com>
2024-05-05 15:02:19 +0530

    Remove char of str1 from str2

---
+++ RemoveChar.java
@@ -0,0 +1,37 @@
+// Given two strings string1 and string2, remove those characters from first string(string1) 
+//which are present in second string(string2). Both the strings are different and contain 
+//only lowercase characters.
+// NOTE: Size of  first string is always greater than the size of  second string( |string1| > |string2|).
+ 
+
+// Example 1:
+
+// Input:
+// string1 = "computer"
+// string2 = "cat"
+// Output: "ompuer"
+// Explanation: After removing characters(c, a, t)
+// from string1 we get "ompuer".
+import java.util.*;
+public class RemoveChar {
+    static String removeChars(String string1, String string2) {
+        HashSet<Character> set = new HashSet<>();
+        StringBuilder res = new StringBuilder();
+        for (char ch : string2.toCharArray()) {
+            set.add(ch);
+        }
+        for (char ch : string1.toCharArray()) {
+            if (!set.contains(ch)) {
+                res.append(ch);
+            }
+        }
+        return res.toString();
+    }
+    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str1 = sc.nextLine();
+        String str2 = sc.nextLine();
+        System.out.println(removeChars(str1,str2));
+    }
+}
