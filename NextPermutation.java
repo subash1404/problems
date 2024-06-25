ac6e9f998d502bc49155301c447633b35d7666fd
Subash V <subashvelusamy10@gmail.com>
2024-06-25 19:54:01 +0530

    next permutation

---
+++ NextPermutation.java
@@ -0,0 +1,66 @@
+import java.util.Scanner;
+
+public class NextPermutation {
+    // A brute force soln would be to find all the possible combinations in a sorted order using recursion
+    // and store them in an array or something. then simply return the next index of the given input
+    // this would take a very large TC (N! * N) for n = 15 -> TC = 15^10(approx)
+
+    // Optimal
+    static public void nextPermutation(int[] arr) {
+        int n = arr.length;
+        int ind = -1;
+        // Find the breakpoint, the point where the dip happens
+        // the array before the breakpoint will be fixed and we will not disturb it
+        for (int i = n - 2; i >= 0; i--) {
+            if (arr[i] < arr[i + 1]) {
+                ind = i;
+                break;
+            }
+        }
+        // if no dips are found , this is the largest possible permutation 
+        // and just reversing the arr will give the answer (minimum possible permutation) 
+        if (ind == -1) {
+            int i = 0, j = n - 1;
+            while (i < j) {
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+                i++;
+                j--;
+            }
+            return;
+        }
+        // then we have to find a element to swap with the from index + 1 -> n 
+        //which is just greater than the arr[index]
+        for (int i = n - 1; i >= 0; i--) {
+            if (arr[ind] < arr[i]) {
+                int temp = arr[ind];
+                arr[ind] = arr[i];
+                arr[i] = temp;
+                break;
+            }
+        }
+        // we have to basically sort the arr from ind+1 -> n
+        // as the array from ind+1 -> n was already in decreasing order just reverse it
+        int i = ind + 1, j = n - 1;
+        while (i < j) {
+            int temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
+            i++;
+            j--;
+        }
+    }
+    public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    int n = sc.nextInt();
+    int[] arr = new int[n];
+    for (int i = 0; i < n; i++) {
+        arr[i] = sc.nextInt();
+    }
+    nextPermutation(arr);
+    for(int i : arr){
+        System.out.print(i+" ");
+    }
+}
+}
---
+++ RearrangeElemBySign.java
@@ -0,0 +1,45 @@
+import java.util.*;
+public class RearrangeElemBySign {
+    static public int[] rearrangeArray(int[] nums) {
+        int[] pos = new int[nums.length / 2];
+        int[] neg = new int[nums.length / 2];
+        int posIndex = 0, negIndex = 0;
+        for (int i : nums) {
+            if (i >= 0)
+                pos[posIndex++] = i;
+            else
+                neg[negIndex++] = i;
+        }
+        for (int i = 0; i < nums.length / 2; i++) {
+            nums[2 * i] = pos[i];
+            nums[2 * i + 1] = neg[i];
+        }
+        return nums;
+    }
+    static public int[] rearrangeArray2(int[] nums) {
+        int[] ans = new int[nums.length];
+        int posIndex = 0, negIndex = 1;
+        for (int i : nums) {
+            if (i >= 0) {
+                ans[posIndex] = i;
+                posIndex = posIndex + 2;
+            } else {
+                ans[negIndex] = i;
+                negIndex = negIndex + 2;
+            }
+        }
+        return ans;
+    }
+    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        int[] ans = rearrangeArray(arr);
+        System.out.println(Arrays.toString(ans));
+        System.out.println(Arrays.toString(rearrangeArray2(arr)));
+    }
+}
